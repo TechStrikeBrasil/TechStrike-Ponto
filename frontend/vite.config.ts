b3865b807a34ad34cb8f62cf4e01b43064ecd290
Michel Alves <michelalvs@gmail.com>
2025-10-29 22:08:37 -0300

    Innital Frontend Implementation

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+//import path from 'path';
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  server:{
+  port:8080,
+  host: "::"
+},
   plugins: [react()],
-})
+
+}));
